082eef15a32a3cdae6837539f2b4fdb8e4cefcb1
yanabirukova <y.birukova@gmail.com>
2023-04-14 20:15:17 +0300

    renew repository

--- app/src/main/java/com/example/cookbook/data/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/cookbook/data/RecipeRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.cookbook.data
 
+import com.example.cookbook.data.mappers.DataToEntityRecipeMapper
+import com.example.cookbook.data.mappers.EntityToDataRecipeMapper
 import com.example.cookbook.data.mappers.ResponseToDataRecipeMapper
 import com.example.cookbook.data.network.RecipeService
 import com.example.cookbook.domain.models.RecipeData
@@ -10,26 +12,49 @@ import javax.inject.Inject
 
 class RecipeRepositoryImpl @Inject constructor(
     private val responseToDataMapper: ResponseToDataRecipeMapper,
+    private val dataToEntityMapper: DataToEntityRecipeMapper,
+    private val entityToDataMapper: EntityToDataRecipeMapper,
     private val service: RecipeService,
-    private val database: RandomRecipesDataBaseSource
+    private val database: DatabaseSource
 ) : RecipeRepository {
 
-    //получаю список рандомных рецептов (нужно ли в базу данных?)
+    override suspend fun refreshDatabaseWithRandomRecipes() {
+        withContext(Dispatchers.IO) {
+            val list = service.getRandomRecipeList().hits?.map {
+                responseToDataMapper(it)
+            } ?: throw Exception()
+            val listOfEntities = list.map {
+                dataToEntityMapper(it)
+            }
+            database.insertAllRecipe(*listOfEntities.toTypedArray())
+        }
+    }
+
     override suspend fun getRandomRecipeList(): List<RecipeData> {
         return withContext(Dispatchers.IO) {
-            val list = service.getRandomRecipeList().execute()
-                .body()?.hits?.map {
-                    responseToDataMapper(it)
-                } ?: throw Exception()
+            val list = database.getAllRecipes().map { entityToDataMapper(it) }
             list
         }
     }
 
     override suspend fun getRecipeListBySearching(q: String): List<RecipeData> {
         return withContext(Dispatchers.IO) {
-            service.getRecipeListBySearching(q).execute()
-                .body()?.hits?.map { responseToDataMapper(it) }
-                ?: throw Exception()
+            service.getRecipeListBySearching(q).hits?.map {
+                responseToDataMapper(it)
+            } ?: throw Exception()
+        }
+    }
+
+    override suspend fun updateIsFavorite(recipe: RecipeData) {
+        withContext(Dispatchers.IO) {
+            database.updateIsFavorite(recipe.isFavorite, recipe.id)
+        }
+    }
+
+    override suspend fun getFavoriteRecipeList(): List<RecipeData> {
+        return withContext(Dispatchers.IO) {
+            val list = database.getFavoriteRecipes(true).map { entityToDataMapper(it) }
+            list
         }
     }
 }
--- app/src/main/java/com/example/cookbook/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/cookbook/domain/repository/RecipeRepository.kt
@@ -4,7 +4,13 @@ import com.example.cookbook.domain.models.RecipeData
 
 interface RecipeRepository {
 
+    suspend fun refreshDatabaseWithRandomRecipes()
+
     suspend fun getRandomRecipeList(): List<RecipeData>
 
     suspend fun getRecipeListBySearching(q: String): List<RecipeData>
+
+    suspend fun updateIsFavorite(recipe: RecipeData)
+
+    suspend fun getFavoriteRecipeList(): List<RecipeData>
 }
